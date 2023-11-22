ef95195822222fb258a3bc899bde105075aae2f5
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-22 12:40:07 -0500

    checkauth

--- app/dashboard/CheckAuth.js
+++ app/dashboard/CheckAuth.js
@@ -1,20 +1,31 @@
 'use client'
-import { useEffect, useContext } from 'react'
-import { useRouter } from 'next/navigation'
+import { useEffect, useContext , setUser} from 'react'
+import { useRouter, usePathname } from 'next/navigation'
 import { AuthContext } from '@/context/AuthContext'
+
 
 export default function CheckAuth() {
 
   const { user } = useContext(AuthContext)
 
   const router = useRouter()
+  const pathname = usePathname()
 
   useEffect(() => {
-    if(!user) {
-      // redirect to login page
-      router.push('/login')
-    }
-  }, [user])
+    if(!user && pathname.startsWith('/dashboard')) {
+        const _user = sessionStorage.getItem('user')
+        if(_user) {
+            setUser(JSON.parse(_user))
+        } else {
+            // redirect to login page
+            router.push('/login')
+          }
+        }
+      if(user && pathname.startsWith('/login')) {
+        // redirect toc dashboard page
+        router.push('/dashboard')
+      }
+    }, [user, pathname])
 
   return null
 }
--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,13 +1,37 @@
 'use client'
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext(null)
+
 export default function AuthProvider({ children }) {
+
     const [user, setUser] = useState(null)
 
+    useEffect(() => {
+        if (!user) {
+            const _user = sessionStorage.getItem("user")
+            if (_user) {
+                setUser(JSON.parse(_user))
+            }
+        }
+    }, [])
+
+
+    const _setUser = (data) => {
+        if (data) {
+            sessionStorage.setItem("user", JSON.stringify(data))
+
+        } else {
+            sessionStorage.removeItem("user")
+
+        } setUser(data)
+
+    }
+
+
     return (
-        <AuthContext.Provider value={{ user, setUser}}>
+        <AuthContext.Provider value={{ user, setUser: _setUser }}>
             {children}
         </AuthContext.Provider>
     )
